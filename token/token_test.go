ed9031edab56b527ca103068e92817f6714a9e3e
Linkinlog <dahlton@dahlton.org>
2023-05-23 22:09:52 -0400

    test(token): 100% Coverage of token.go

---
+++ token/token_test.go
@@ -0,0 +1,36 @@
+package token
+
+import "testing"
+
+func TestLookupIdent(t *testing.T) {
+	type args struct {
+		ident string
+	}
+	tests := []struct {
+		name string
+		args args
+		want TokenType
+	}{
+		{
+			name: "Test_LookupIdent_fn",
+			args: args{
+				ident: "fn",
+			},
+			want: FUNCTION,
+		},
+		{
+			name: "Test_LookupIdent_IDENT",
+			args: args{
+				ident: "foo",
+			},
+			want: IDENT,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := LookupIdent(tt.args.ident); got != tt.want {
+				t.Errorf("LookupIdent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
