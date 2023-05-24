a70eb7267333c417c170c65e74b920a2e817d11d
Linkinlog <dahlton@dahlton.org>
2023-05-23 22:01:22 -0400

    test(lexer): Flush out code coverage

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/Linkinlog/MagLang/token"
@@ -107,5 +108,352 @@ func TestNextTokenFull(t *testing.T) {
 			t.Fatalf("tests[%d] - tokenLiteral wrong, exptected=%q, received=%q",
 				idx, tt.expectedLiteral, token.Literal)
 		}
+	}
+}
+
+func TestNew(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Lexer
+	}{
+		{
+			name: "Test_New_01",
+			args: args{input: "let foo = 5;"},
+			want: &Lexer{
+				input:        "let foo = 5;",
+				position:     0,
+				readPosition: 1,
+				char:         byte('l'),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLexer_readChar(t *testing.T) {
+	type fields struct {
+		input        string
+		position     int
+		readPosition int
+		char         byte
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "Test_readChar_01",
+			fields: fields{
+				input:        "let foo = 5;",
+				position:     0,
+				readPosition: 1,
+				char:         byte('l'),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Lexer{
+				input:        tt.fields.input,
+				position:     tt.fields.position,
+				readPosition: tt.fields.readPosition,
+				char:         tt.fields.char,
+			}
+			l.readChar()
+		})
+	}
+}
+
+func TestLexer_NextToken(t *testing.T) {
+	type fields struct {
+		input        string
+		position     int
+		readPosition int
+		char         byte
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		wantToke token.Token
+	}{
+		{
+			name: "Test_NextToken_ILLEGAL",
+			fields: fields{
+				input:        "!foo",
+				position:     0,
+				readPosition: 1,
+				char:         byte('!'),
+			},
+			wantToke: token.Token{
+				Type:    token.ILLEGAL,
+				Literal: "!",
+			},
+		},
+		{
+			name: "Test_NextToken_IDENT",
+			fields: fields{
+				input:        "foo",
+				position:     0,
+				readPosition: 1,
+				char:         byte('f'),
+			},
+			wantToke: token.Token{
+				Type:    token.IDENT,
+				Literal: "foo",
+			},
+		},
+		{
+			name: "Test_NextToken_IDENT",
+			fields: fields{
+				input:        "let",
+				position:     0,
+				readPosition: 1,
+				char:         byte('l'),
+			},
+			wantToke: token.Token{
+				Type:    token.LET,
+				Literal: "let",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Lexer{
+				input:        tt.fields.input,
+				position:     tt.fields.position,
+				readPosition: tt.fields.readPosition,
+				char:         tt.fields.char,
+			}
+			if gotToke := l.NextToken(); !reflect.DeepEqual(gotToke, tt.wantToke) {
+				t.Errorf("Lexer.NextToken() = %v, want %v", gotToke, tt.wantToke)
+			}
+		})
+	}
+}
+
+func TestLexer_readIdentifier(t *testing.T) {
+	type fields struct {
+		input        string
+		position     int
+		readPosition int
+		char         byte
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "Test_readIdentifier_FUNCTION",
+			fields: fields{
+				input:        "function",
+				position:     0,
+				readPosition: 1,
+				char:         byte('f'),
+			},
+			want: "function",
+		},
+		{
+			name: "Test_readIdentifier_LET",
+			fields: fields{
+				input:        "let",
+				position:     0,
+				readPosition: 1,
+				char:         byte('l'),
+			},
+			want: "let",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Lexer{
+				input:        tt.fields.input,
+				position:     tt.fields.position,
+				readPosition: tt.fields.readPosition,
+				char:         tt.fields.char,
+			}
+			if got := l.readIdentifier(); got != tt.want {
+				t.Errorf("Lexer.readIdentifier() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLexer_readNumber(t *testing.T) {
+	type fields struct {
+		input        string
+		position     int
+		readPosition int
+		char         byte
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "Test_readNumber_5",
+			fields: fields{
+				input:        "5",
+				position:     0,
+				readPosition: 1,
+				char:         byte('5'),
+			},
+			want: "5",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Lexer{
+				input:        tt.fields.input,
+				position:     tt.fields.position,
+				readPosition: tt.fields.readPosition,
+				char:         tt.fields.char,
+			}
+			if got := l.readNumber(); got != tt.want {
+				t.Errorf("Lexer.readNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_newToken(t *testing.T) {
+	type args struct {
+		tokenType token.TokenType
+		char      byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want token.Token
+	}{
+		{
+			name: "Test_newToken_ASSIGN",
+			args: args{
+				tokenType: token.ASSIGN,
+				char:      '=',
+			},
+			want: token.Token{
+				Type:    token.ASSIGN,
+				Literal: "=",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newToken(tt.args.tokenType, tt.args.char); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newToken() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isLetter(t *testing.T) {
+	type args struct {
+		char byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Test_isLetter_5",
+			args: args{
+				char: '5',
+			},
+			want: false,
+		},
+		{
+			name: "Test_isLetter_a",
+			args: args{
+				char: 'a',
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isLetter(tt.args.char); got != tt.want {
+				t.Errorf("isLetter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isDigit(t *testing.T) {
+	type args struct {
+		char byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Test_isDigit_a",
+			args: args{
+				char: 'a',
+			},
+			want: false,
+		},
+		{
+			name: "Test_isDigit_0",
+			args: args{
+				char: '0',
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isDigit(tt.args.char); got != tt.want {
+				t.Errorf("isDigit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLexer_skipWhitespace(t *testing.T) {
+	type fields struct {
+		input        string
+		position     int
+		readPosition int
+		char         byte
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "Test_SkipWhitespace_01",
+			fields: fields{
+				input:        "let     foo = 5;",
+				position:     0,
+				readPosition: 1,
+				char:         byte('l'),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &Lexer{
+				input:        tt.fields.input,
+				position:     tt.fields.position,
+				readPosition: tt.fields.readPosition,
+				char:         tt.fields.char,
+			}
+			l.skipWhitespace()
+		})
 	}
 }
